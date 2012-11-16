fdf2456dc0b4ee7969332c889ea5bc73a63554d0
Brendan Hill <bhill@godaddy.com>
2012-11-16 18:19:02 -0500

    basic unittests in place

---
+++ unittests.js
@@ -0,0 +1,10 @@
+/*global Ember $ test ok*/
+
+module("Module 1 :: Basic Tests", {
+   setup: function (){},
+   teardown: function () {},
+});
+
+test("test 1", function() {
+    ok(true, 'test assertion');
+});
